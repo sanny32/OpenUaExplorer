4a24cc52043357dd4d335a040ce476ad97b4137c
Ananev Alexandr <mail@ananev.org>
2026-06-11 09:56:47 +0300

    feat: Add resource script for Windows application with icon and version info

--- src/cmake/target_windows.cmake
+++ src/cmake/target_windows.cmake
@@ -3,5 +3,13 @@ function(ouaexp_configure_target_windows target_name)
         target_compile_options(${target_name} PRIVATE /utf-8)
     endif()
 
+    set(ICON_FILE "${CMAKE_CURRENT_SOURCE_DIR}/res/icons/light/app.ico")
+    configure_file(
+        "${CMAKE_CURRENT_SOURCE_DIR}/ouaexp.rc.in"
+        "${PROJECT_BINARY_DIR}/ouaexp.rc"
+        @ONLY
+    )
+
+    target_sources(${target_name} PRIVATE "${PROJECT_BINARY_DIR}/ouaexp.rc")
     set_target_properties(${target_name} PROPERTIES WIN32_EXECUTABLE ON)
 endfunction()
